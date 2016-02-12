54b75a636704faf05ef647c16fec22965aa58be3
Kihira <kihira@kihirakreations.co.uk>
2016-02-12 18:44:24 +0000

    Initial commit

---
+++ Assets/Scripts/HandInfoPanel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandInfoPanel : MonoBehaviour
+{
+    [SerializeField] private HandController handController;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool faceCamera;
+    [SerializeField] private float tolerance = 0.1f;
+    [SerializeField] private float followSpeed = 0.1f;
+
+    [SerializeField] private float retrackTime = 2f;
+    private float detachedTime = 0f;
+
+    private HandModel hand;
+    private bool leftHand;
+
+	void Update ()
+	{
+        // Attempt to reattach if possible
+	    if (hand == null)
+	    {
+	        if (detachedTime > retrackTime)
+	        {
+	            Debug.Log("Failed to retrack correct hand, deactivating");
+                Disable();
+                return;
+	        }
+	        foreach (var graphicHand in handController.GetAllGraphicsHands())
+	        {
+	            if (graphicHand.GetLeapHand().IsLeft == leftHand)
+	            {
+                    hand = graphicHand;
+                    detachedTime = 0f;
+                    break;
+	            }
+	        }
+	        detachedTime += Time.deltaTime;
+	    }
+        if (hand == null) return;
+
+        // Less jittery to manually transform the position then add it as a child of the palm
+        Vector3 newPos = hand.GetPalmPosition() + (hand.palm.up * 2f);
+        if (Math.Abs(newPos.sqrMagnitude - transform.position.sqrMagnitude) > tolerance)
+        {
+            transform.Translate((newPos-transform.position)*followSpeed);
+        }
+        // TODO FIXME
+        //if (faceCamera) transform.LookAt(Camera.main.transform);
+	}
+
+    private void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Attach this panel to a certain hand
+    /// The hand side is stored for future reference to re-attach to incase of temporary loss of tracking
+    /// </summary>
+    /// <param name="handModel">The HandModel to attach to</param>
+    public void Attach(HandModel handModel)
+    {
+        hand = handModel;
+        leftHand = handModel.GetLeapHand().IsLeft;
+    }
+}
---
+++ Assets/Scripts/WallInteractable.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class WallInteractable : MonoBehaviour
+{
+
+    [SerializeField] private GameObject handInfoPanel;
+
+    private int currHandTouching = -1;
+
+    HandModel GetHand(Transform other)
+    {
+        HandModel hand = null;
+        if (other == null)
+        {
+            return null;
+        }
+        hand = other.GetComponent<HandModel>();
+        return hand ?? GetHand(other.transform.parent);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // TODO way to allow replaying?
+        if (currHandTouching != -1) return;
+
+        var hand = GetHand(other.transform);
+        if (hand == null) return;
+
+        var handID = hand.GetLeapHand().Id;
+        currHandTouching = handID;
+
+        // Attach hand info panel and activate
+        handInfoPanel.GetComponent<HandInfoPanel>().Attach(hand);
+        handInfoPanel.SetActive(true);
+
+        Debug.Log("Hand activated wall panel");
+    }
+
+/*    void OnTriggerStay(Collider other)
+    {
+        if (currHandTouching == -1) return;
+        else Debug.Log("Hand colliding");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        HandModel hand_model = GetHand(other.transform);
+        if (hand_model != null && hand_model.GetLeapHand().Id == currHandTouching)
+        {
+            Debug.Log("Hand leave " + currHandTouching);
+            currHandTouching = -1;
+        }
+    }*/
+}
