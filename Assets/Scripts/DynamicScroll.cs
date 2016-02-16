92be5544f05a9275bca0a6d3d96298a95ae298ca
Kihira <kihira@kihirakreations.co.uk>
2016-02-16 12:24:57 +0000

    Allow text panel to resize much nicer by following unity standards Improve movement of panel by adding smoothing

---
+++ Assets/Scripts/DynamicScroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DynamicScroll : ScrollBase {
+
+    void Start()
+    {
+        base.Start();
+
+        BoxCollider collider = GetComponent<BoxCollider>();
+        RectTransform parentTransform = (RectTransform)transform.parent;
+        collider.size = new Vector3(parentTransform.rect.width, parentTransform.rect.height, 0.5f);
+
+        // Hacky fix for ContentSizeFitter
+        Invoke("ContentBoundUpdate", 0.1f);
+    }
+
+    // Hacky fix for ContentSizeFitter
+    void ContentBoundUpdate()
+    {
+        var heightMid = ContentTransform.GetComponent<RectTransform>().rect.height/2f;
+        ContentTopBound.localPosition = new Vector3(0f, heightMid, 0f);
+        ContentBottomBound.localPosition = new Vector3(0f, -heightMid, 0f);
+        ContentTransform.localPosition = new Vector3(0f, -heightMid, 0f);
+    }
+}
--- Assets/Scripts/HandInfoPanel.cs
+++ Assets/Scripts/HandInfoPanel.cs
@@ -6,7 +6,6 @@ using Leap;
 public class HandInfoPanel : MonoBehaviour
 {
     [SerializeField] private HandController handController;
-    [SerializeField] private Vector3 offset;
     [SerializeField] private bool faceCamera;
     [SerializeField] private float tolerance = 0.1f;
     [SerializeField] private float followSpeed = 0.1f;
@@ -17,39 +16,49 @@ public class HandInfoPanel : MonoBehaviour
     private HandModel hand;
     private bool leftHand;
 
-	void Update ()
-	{
+    private float playingTime = 0f;
+
+    private void Update()
+    {
         // Attempt to reattach if possible
-	    if (hand == null)
-	    {
-	        if (detachedTime > retrackTime)
-	        {
-	            Debug.Log("Failed to retrack correct hand, deactivating");
+        if (hand == null)
+        {
+            if (detachedTime > retrackTime)
+            {
+                Debug.Log("Failed to retrack correct hand, deactivating");
                 Disable();
                 return;
-	        }
-	        foreach (var graphicHand in handController.GetAllGraphicsHands())
-	        {
-	            if (graphicHand.GetLeapHand().IsLeft == leftHand)
-	            {
+            }
+            foreach (var graphicHand in handController.GetAllGraphicsHands())
+            {
+                if (graphicHand.GetLeapHand().IsLeft == leftHand)
+                {
                     hand = graphicHand;
                     detachedTime = 0f;
                     break;
-	            }
-	        }
-	        detachedTime += Time.deltaTime;
-	    }
+                }
+            }
+            detachedTime += Time.deltaTime;
+        }
         if (hand == null) return;
 
+        // Display downloading message if required
+
+
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
         // Less jittery to manually transform the position then add it as a child of the palm
         Vector3 newPos = hand.GetPalmPosition() + (hand.palm.up * 2f);
         if (Math.Abs(newPos.sqrMagnitude - transform.position.sqrMagnitude) > tolerance)
         {
-            transform.Translate((newPos-transform.position)*followSpeed);
+            transform.Translate((newPos - transform.position) * followSpeed);
         }
         // TODO FIXME
         //if (faceCamera) transform.LookAt(Camera.main.transform);
-	}
+    }
 
     private void Disable()
     {
@@ -65,5 +74,13 @@ public class HandInfoPanel : MonoBehaviour
     {
         hand = handModel;
         leftHand = handModel.GetLeapHand().IsLeft;
+    }
+
+    public void Attach(HandModel handModel, string text)
+    {
+        Attach(handModel);
+
+        playingTime = 0f;
+
     }
 }
