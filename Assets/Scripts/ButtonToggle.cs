8835e16a40c907a88508c2fac1bd4bd5d9a48853
Kihira <kihira@kihirakreations.co.uk>
2016-02-28 20:06:37 +0000

    Add non functioning start/stop button

---
+++ Assets/Scripts/ButtonToggle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using LMWidgets;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// PUT BUTTON IN CHILD OF EMPTY GAME OBJECT AND SET PARENT GAME OBJECT OF POSITION WHERE YOU WANT IT TO BE
+/// Works around "issue" with default Leap button behaviour that resets x/y values
+/// </summary>
+[RequireComponent(typeof (Button), typeof (Image))]
+public class ButtonToggle : ButtonToggleBase
+{
+
+    // TODO support multiple transition types in the future?
+    public override void ButtonTurnsOn()
+    {
+        Button button = GetComponent<Button>();
+        if (button.transition == Selectable.Transition.SpriteSwap)
+        {
+            GetComponent<Image>().overrideSprite = GetComponent<Button>().spriteState.pressedSprite;
+        }
+        GetComponent<Button>().OnPointerClick(new PointerEventData(null));
+    }
+
+    public override void ButtonTurnsOff()
+    {
+        Button button = GetComponent<Button>();
+        if (button.transition == Selectable.Transition.SpriteSwap)
+        {
+            GetComponent<Image>().overrideSprite = null;
+        }
+        GetComponent<Button>().OnPointerClick(new PointerEventData(null));
+    }
+}
