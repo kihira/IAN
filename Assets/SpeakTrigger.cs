07272ef84b545a728d51e8c3bc16e8654153a292
Kihira <kihira@kihirakreations.co.uk>
2016-03-02 10:12:32 +0000

    Add story checkpoints by Ross

---
+++ Assets/SpeakTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeakTrigger : MonoBehaviour {
+
+	public AudioClip voiceClip;
+    private AudioSource audioSource;
+	private bool hasPlayed = false;
+
+	void Start () {
+        audioSource = GameObject.Find("Player/Hand Mount").GetComponent<AudioSource>();
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+		Debug.Log ("The Trigger functions");
+		if (!hasPlayed) {
+			Debug.Log ("Audio hasn't been played");
+		    audioSource.clip = voiceClip;
+            audioSource.Play();
+		}
+	}
+}
