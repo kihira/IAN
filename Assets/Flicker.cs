a7411a20e0d3ebd43e57f27534bd04f65a87b37e
Kihira <kihira@kihirakreations.co.uk>
2016-03-21 23:00:07 +0000

    Add tablet entries and fix lighting on hands

---
+++ Assets/Flicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour
+{
+
+    private float lastChange = 0;
+    private float start;
+    private Light light;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    light = GetComponent<Light>();
+	    start = light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (lastChange > 2f)
+	    {
+	        light.intensity = Math.Abs(light.intensity) < 0.1f ? start : 0;
+	        lastChange = 0;
+	    }
+	    lastChange += Time.deltaTime;
+	}
+}
