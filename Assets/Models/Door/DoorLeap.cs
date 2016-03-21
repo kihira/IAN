f930cad4f20d95775737a2edacba0e22c263c631
shinigamirei <shinigamireyas@googlemail.com>
2016-03-21 21:04:22 +0000

    added push door
    implimented push door with animation and (hopeful) leap interaction

---
+++ Assets/Models/Door/DoorLeap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Leap;
+
+public class DoorLeap : MonoBehaviour {
+
+	bool Open; 
+
+	HandModel GetHand(Transform other)
+	{
+		HandModel hand = null;
+		if (other == null)
+		{
+			return null;
+		}
+		hand = other.GetComponent<HandModel>();
+		return hand ?? GetHand(other.transform.parent);
+	}
+
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (Open == true)
+			return;
+
+		HandModel hand = GetHand (collider.transform);
+		if (collider.gameObject.tag == "Player" || hand == null || hand.GetLeapHand ().IsLeft) 
+		{
+			this.GetComponent<Animator> ().SetTrigger ("Open");
+			Open = true;
+		}	
+			return;
+	}
+
+	void
+}
