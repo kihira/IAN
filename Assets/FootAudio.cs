9c2ed51a0fe8a16eab4f5cbb40a064506a6dc406
Kihira <kihira@kihirakreations.co.uk>
2016-03-30 10:04:01 +0100

    Audio fixes as well as switch light baking to manual

--- Assets/FootAudio.cs
+++ Assets/FootAudio.cs
@@ -7,6 +7,7 @@ public class FootAudio: MonoBehaviour
     CharacterController cc;
     public AudioClip otherClip;
     [SerializeField] AudioSource footstep;
+    private float lastPlayed;
 
     void Start()
     {
@@ -15,11 +16,12 @@ public class FootAudio: MonoBehaviour
 
     void Update()
     {
-        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && footstep.isPlaying == false)
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && footstep.isPlaying == false && Time.time - lastPlayed > 0.5f)
         {
             footstep.volume = Random.Range(0.01f, 0.05f);
             footstep.pitch = Random.Range(0.8f, 1.1f);
             footstep.Play();
+            lastPlayed = Time.time;
         }
 
     }
